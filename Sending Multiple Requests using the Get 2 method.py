59e5943ca539443b54114cdfa3d47bbc44d4cd7c
Mihail Komiser <komiser@gmail.com>
2024-11-02 16:59:05 +0300

    запрос категории от пользователя

---
+++ Sending Multiple Requests using the Get 2 method.py
@@ -0,0 +1,33 @@
+import requests
+
+
+class TestCreateJokeCategory():
+
+    def __init__(self, base_url):
+        self.categories = None
+        self.base_url = base_url
+
+    def list_of_all_categories(self):
+        url = f"{self.base_url}/categories"
+        result = requests.get(url)
+        self.categories = result.json()
+        print(self.categories)
+
+    def test_joke_for_user_selected_category(self):
+        # Шаг 1: Запрос категории от пользователя
+        user_category = input("Выберите категорию для получения шутки: ")
+
+        # шаг 2: проверка, что выбранная пользователем категория, есть в списке категорий
+        assert user_category in self.categories, f"Категория '{user_category}' не найдена в списке категорий: {self.categories}"
+        print("Данная категория есть в списке всех категорий\n")
+
+        # шаг 2: Запрос одной шутки по категории, запрошенную пользователем
+        response = requests.get(f"{self.base_url}/random", params={"category": user_category})
+        data = response.json()
+        joke = data['value']
+        print(joke)
+
+
+start = TestCreateJokeCategory('https://api.chucknorris.io/jokes')
+start.list_of_all_categories()
+start.test_joke_for_user_selected_category()
